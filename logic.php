c33c97f0f463a82e0c0df4037237c2cc9b5a9f1c
Alexander Schmidt <alexander.schmidt@edvas.de>
2014-11-25 08:23:31 +0100

    Create logic.php

---
+++ logic.php
@@ -0,0 +1,19 @@
+<?php defined( '_JEXEC' ) or die;
+
+// variables
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument();
+$menu = $app->getMenu();
+$active = $app->getMenu()->getActive();
+$params = $app->getParams();
+$pageclass = $params->get('pageclass_sfx');
+$tpath = $this->baseurl.'/templates/'.$this->template;
+
+// generator tag
+$this->setGenerator(null);
+
+// template js
+$doc->addScript($tpath.'/js/logic.js');
+
+// template css
+$doc->addStyleSheet($tpath.'/css/template.css.php');
